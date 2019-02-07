94a66a05f346ebf2d4ce436d90135a615087ba89
vksingh26 <vksingh20@outlook.com>
2019-02-07 12:23:48 +0530

    Added test case using Mocha

--- src/script/calc.js
+++ src/script/calc.js
@@ -1,8 +1,14 @@
 let add = function(a, b){
+    if(!a || !b){
+        throw new Error("expected values are undefined for add  method !")
+    }
     return a + b;
 }
 
 let substract = function(a, b){
+    if(!a || !b){
+        throw new Error("expected values are undefined for substract method !")
+    }
     return a - b;
 }
 
--- src/test/calc.spec.js
+++ src/test/calc.spec.js
@@ -1,6 +1,31 @@
 const assert = require('assert');
-const calci = require('../script/calci');
+const calc = require('../script/calc');
 
-describe("Calculator", function() {
-    
+describe('Calculator Test suit', function() {
+  describe('Testing add function add(a, b)', () => {
+    it('If I pass add(5, 5), it should return 10', ()=> {
+        let actual = calc.add(5, 5);
+        let expected = 10;
+        assert.equal(actual, expected);
+    })
+    it('If I donot pass anything in add(), it should throw error', () => {
+        assert.throws(() => {
+            calc.add();
+        })
+    });
+  });
+
+  describe.only('Testing substract function substract(a, b)', () => {
+      it('Pass substract(10, 5), it should return 5', () => {
+            actual = calc.substract(10, 5);
+            expected = 5;
+            assert.equal(actual, expected);
+      });
+
+      it('Donot pass anything in substract(), it should throw error', () => {
+            assert.throws(() =>{
+                calc.substract();
+            });
+      });
+  });
 });
