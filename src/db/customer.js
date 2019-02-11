f84d1b1079446414d300a2bceeea4553634daa24
vksingh26 <vksingh20@outlook.com>
2019-02-11 16:09:13 +0530

    added customer db

---
+++ src/db/customer.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id:1,
+        FirstName: "Vikash",
+        LastName: "Singh",
+        Email: "vks0626@gmail.com",
+        Mobile: "7259669313",
+        registered_on: '2019-02-11 00:00:00'
+    },
+    {
+        id:2,
+        FirstName: "Deepak",
+        LastName: "Chouhan",
+        Email: "deepak.c@gmail.com",
+        Mobile: "9874563210",
+        registered_on: '2019-02-11 00:00:00'
+    }
+]
